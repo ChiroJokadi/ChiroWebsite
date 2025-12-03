7bc4c654a57e43b5c44a6f6f642210cf698953b9
Chiro Jokadi <chiro.jokadi1@gmail.com>
2025-12-03 19:38:19 +0100

    navbar fix

--- front-end/pages/leiding.tsx
+++ front-end/pages/leiding.tsx
@@ -921,8 +921,8 @@ const Leiding: React.FC = () => {
                           Naam: Robbe Schiffeleers
                         </div>
                         <div className="font-semibold text-lg">
-                          Ik ben 5de jaar leider (eerder: rakkers,kerels,
-                          speelclub,toppers)
+                          Ik ben 5de jaar leider (eerder: rakkers, kerels,
+                          speelclub, toppers)
                         </div>
                         <div className="font-semibold text-lg">
                           Studies: Revalidatiewetenschappen & Kinesitherapie
