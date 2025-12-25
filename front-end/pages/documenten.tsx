bc299f9a01431bc3ea2e27bf0cc1fbabcc92ebf0
ChiroJokadi <chiro.jokadi1@gmail.com>
2025-12-25 12:13:06 +0100

    Update documenten.tsx

--- front-end/pages/documenten.tsx
+++ front-end/pages/documenten.tsx
@@ -34,7 +34,7 @@ const Documenten: React.FC = () => {
             <div className="rounded-2xl shadow overflow-hidden bg-white">
               <img
                 src="/images/prikbord.jpeg"
-                alt="Prikbord brief"
+                alt="Prikbord"
                 className="w-full h-[420px] object-cover"
               />
             </div>
